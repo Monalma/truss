01d39fdb04c46a35e1dcd4924d02069232b48e6b
monalma <mmahaja@purdue.edu>
2021-03-18 19:33:35 -0400

    Initial Commit

---
+++ trussAppTests/trussAppTests.swift
@@ -0,0 +1,33 @@
+//
+//  trussAppTests.swift
+//  trussAppTests
+//
+//  Created by Monal mahajan on 3/18/21.
+//
+
+import XCTest
+@testable import trussApp
+
+class trussAppTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
